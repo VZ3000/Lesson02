e6904b212fdc6a38133c8911b04f804f9971bcbb
Me1 <Me1@DESKTOP-02SL8LJ>
2022-09-23 21:42:03 +0300

    1

---
+++ Lesson02_04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson02_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string schoolName;
+            string className;
+            int numberOfStudents;
+            schoolName = "CodeAcademy";
+            className = ".Net";
+            numberOfStudents = 25;
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine($"School name: {schoolName}, Class Name: {className}, Number of students: {numberOfStudents}, Current Date: {now}");
+
+            DateTime courseStartDate = DateTime.Parse("2022-09-05");
+            DateTime courseEndDate = DateTime.Parse("2023-03-01");
+            var workDaysCount = ((courseEndDate - courseStartDate) / 7 * 4).Days;
+            Console.WriteLine("Number of course days:" + workDaysCount);
+
+        }
+    }
+}
